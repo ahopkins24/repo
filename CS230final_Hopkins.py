9eeb3d33a8304cd8cd300bd0188e6f3df25422e8
ahopkins24 <95844311+ahopkins24@users.noreply.github.com>
2021-12-09 03:04:10 -0500

    Update CS230final_Hopkins.py

--- CS230final_Hopkins.py
+++ CS230final_Hopkins.py
@@ -8,8 +8,9 @@ This program takes data from all NCAA Division 1 Football stadiums and compiles 
 The user can sort by division, conference, and popular rivalries to get more information on the stadiums all 253 NCAA
 Division 1 Football teams play in.
 """
-
-python -m pip install folium
+>>> exit()
+pip install folium
+python3
 import os
 import webbrowser
 import folium
